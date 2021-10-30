b4f4a2cb2bb30f36f70446c94efc07b8120fc140
schim613 <uximin@naver.com>
2021-10-31 01:53:53 +0900

    얼추 디자인 시안대로  완성

--- index.ssg.php
+++ index.ssg.php
@@ -50,9 +50,9 @@
           <div class="swiper-slide">
             <a href="#" class="donut-list flex flex-col items-center justify-center">
               <img src="resource/img/no-1.png" alt="">
-              <div class="about-donut flex flex-col items-center justify-center">
+              <div class="about-donut no-1 flex flex-col items-center justify-center">
                 <div>No.1  2020년 167,102개 판매!</div>
-                <div>글레이즈 도넛</div>
+                <div class="title">글레이즈 도넛</div>
                 <div>Glazed Raised</div>
               </div>
             </a>
@@ -60,9 +60,9 @@
           <div class="swiper-slide">
             <a href="#" class="donut-list flex flex-col items-center justify-center">
               <img src="resource/img/no-2.png" alt="">
-              <div class="about-donut flex flex-col items-center justify-center">
+              <div class="about-donut no-2 flex flex-col items-center justify-center">
                 <div>No.2  2020년 108,898개 판매!</div>
-                <div>버터 크럼 도넛</div>
+                <div class="title">버터 크럼 도넛</div>
                 <div>Butter Crumb Raised</div>
               </div>
             </a>
@@ -70,9 +70,9 @@
           <div class="swiper-slide">
             <a href="#" class="donut-list flex flex-col items-center justify-center">
               <img src="resource/img/no-3.png" alt="">
-              <div class="about-donut flex flex-col items-center justify-center">
+              <div class="about-donut no-3 flex flex-col items-center justify-center">
                 <div>No.3  2020년 71,206개 판매!</div>
-                <div>누텔라 도넛</div>
+                <div class="title">누텔라 도넛</div>
                 <div>Nutella Raised</div>
               </div>
             </a>
@@ -80,9 +80,9 @@
           <div class="swiper-slide">
             <a href="#" class="donut-list flex flex-col items-center justify-center">
               <img src="resource/img/no-4.png" alt="">
-              <div class="about-donut flex flex-col items-center justify-center">
+              <div class="about-donut no-4 flex flex-col items-center justify-center">
                 <div></div>
-                <div>가나슈 도넛</div>
+                <div class="title">가나슈 도넛</div>
                 <div>White Ganache Raised w/ CHOC</div>
               </div>
             </a>
@@ -90,9 +90,9 @@
           <div class="swiper-slide">
             <a href="#" class="donut-list flex flex-col items-center justify-center">
               <img src="resource/img/no-5.png" alt="">
-              <div class="about-donut flex flex-col items-center justify-center">
+              <div class="about-donut no-5 flex flex-col items-center justify-center">
                 <div></div>
-                <div>엠앤엠 도넛</div>
+                <div class="title">엠앤엠 도넛</div>
                 <div>M&M’s Raised</div>
               </div>
             </a>
@@ -100,9 +100,9 @@
           <div class="swiper-slide">
             <a href="#" class="donut-list flex flex-col items-center justify-center">
               <img src="resource/img/no-6.png" alt="">
-              <div class="about-donut flex flex-col items-center justify-center">
+              <div class="about-donut no-6 flex flex-col items-center justify-center">
                 <div></div>
-                <div>카라멜 코코넛 도넛</div>
+                <div class="title">카라멜 코코넛 도넛</div>
                 <div>Caramel Raised w CHOC/COCO</div>
               </div>
             </a>
@@ -150,7 +150,7 @@
       </div>
     </div>
     <div class="flex">
-      <div style="margin-top:241px; margin-left:255px; margin-right:70px;">
+      <div style="margin-top:241px; margin-left:220px; margin-right:70px;">
         <div class="title mb-5">클래식부터 프리미엄까지 다양한 도넛</div>
         <div class="content">
           랜디스 도넛에는 클래식 도넛부터 프리미엄 도넛까지
@@ -165,32 +165,69 @@
   <!-- 메인페이지-메뉴 박스 -->
   <div class="menu-box site-wrap">
     <div class="flex">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="box-1 flex-center">
+        <img src="resource/img/icon-shop.png" alt="">
+        <div class="text">
+          <div class="flex-center">
+            <div class="title">매장 정보 확인하기</div>
+            <div>Randy’s Donuts Korea</div>
+          </div>
+          <div class="flex-center mt-8">
+            <div>제주 애월점 / 서울 연남점</div>
+            <div>대구 동성로점 / 서울 가로수길점</div>
+          </div>
+        </div>
+      </div>
+      <div class="box-2 flex-center">
+        <img src="resource/img/icon-newspaper.png" alt="">
+        <div class="text">
+          <div class="flex-center">
+            <div class="title">브랜드 소식</div>
+            <div>News & Notice</div>
+          </div>
+          <div class="flex-center mt-8">
+            <div>랜디스 도넛의</div>
+            <div>새로운 소식을 만나보세요!</div>
+          </div>
+        </div>
+      </div>
+      <div class="box-3 flex-center">
+        <img src="resource/img/icon-gift.png" alt="">
+        <div class="text flex-center">
+          <div class="flex-center">
+            <div class="title">이벤트 & 프로모션</div>
+            <div>Event & Promotion</div>
+          </div>
+          <div class="flex-center mt-8">
+            <div>랜디스 도넛의</div>
+            <div>이벤트 및 프로모션을 확인해보세요!</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   
   <!-- 메인페이지-띠 배너 -->
   <div class="band-banner">
-    <div>dd</div>
   </div>
   
   <!-- 메인페이지-인스타그램 -->
   <div class="insta site-wrap">
     <div class="title text-center">INSTAGRAM</div>
     <div class="insta-list flex">
-      <div class="section-1 border-2" style="width:494px; height:494px;"></div>
+      <div class="section-1">
+        <img src="resource/img/insta-1.png" alt="">
+      </div>
       <div class="section-2">
         <div class="flex">
-          <div class="border-2" style="width:230px; height:230px;"></div>
-          <div class="border-2" style="width:230px; height:230px;"></div>
-          <div class="border-2" style="width:230px; height:230px;"></div>
+          <div><img src="resource/img/insta-2.png" alt=""></div>
+          <div><img src="resource/img/insta-3.png" alt=""></div>
+          <div><img src="resource/img/insta-4.png" alt=""></div>
         </div>
         <div class="flex">
-          <div class="border-2" style="width:230px; height:230px;"></div>
-          <div class="border-2" style="width:230px; height:230px;"></div>
-          <div class="border-2" style="width:230px; height:230px;"></div>
+          <div><img src="resource/img/insta-5.png" alt=""></div>
+          <div><img src="resource/img/insta-6.png" alt=""></div>
+          <div><img src="resource/img/insta-7.png" alt=""></div>
         </div>
       </div>
     </div>
